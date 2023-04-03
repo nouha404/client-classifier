0f5c8dbebed3c5c5ee883038db3d8b34cdc55567
unknown <noixstarpro@gmail.com>
2023-04-03 21:55:30 +0000

    Recuperation des données et leurs liaisons avec le model de ML

--- src/Client/views.py
+++ src/Client/views.py
@@ -1,11 +1,22 @@
+import os.path
+from pathlib import Path
+
 from django.shortcuts import render
 from .forms import SignForm
+from joblib import load
+from pathlib import Path
+
+from joblib import load
+import numpy as np
 
 
 # Create your views here.
 def Formulaire(request):
     if request.method == 'POST':
         form = SignForm(request.POST)
+
+        full_path = Path(__file__).parent / 'saveModel' / 'superModel.joblib'
+        model = load(full_path)
 
         if form.is_valid():
             age = form.cleaned_data['age']
@@ -17,7 +28,14 @@ def Formulaire(request):
             loan = form.cleaned_data['loan']
             pdays = form.cleaned_data['pdays']
             previous = form.cleaned_data['previous']
-            return render(request, 'Client/resultat.html')
+
+            # Préparer les données pour la prédiction
+            df = [[int(age), int(marital), int(job), int(education), int(default), int(housing), int(loan), int(pdays),
+                   int(previous)]]
+            eyes_pred = model.predict(df)
+            answer = ['Vous etes un bon client GG' if eyes_pred[0] == 0 else "Vous n'etes pas fiable"]
+
+            return render(request, 'Client/resultat.html', {'answer': answer})
     else:
         form = SignForm()
     return render(request, 'Client/Formulaire.html', {'form': form})
